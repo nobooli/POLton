16e2b4f2380e4ebf617943bba41a4223873ca82c
Merlin Nguyen <jonhenry13241@gmail.com>
2020-08-06 12:57:27 +0700

    add bingo player

--- src/views/BingoGenerator.vue
+++ src/views/BingoGenerator.vue
@@ -2,21 +2,121 @@
   <v-container>
     <v-row align="center">
       <v-col>
-        <v-btn @click="generate">Generate</v-btn>
+        <v-btn @click="generate(null)">Generate</v-btn>
         <v-btn class="ml-4" @click="download">Download</v-btn>
+        <v-btn class="ml-4" @click="reset">Reset board</v-btn>
       </v-col>
     </v-row>
-    <canvas ref="bingoCanvas"></canvas>
+    <canvas ref="bingoCanvas" v-on:click="mouseClick"></canvas>
   </v-container>
 </template>
 <script>
 import bingoLines from "@/assets/bingoLines.json";
 export default {
+  beforeMount() {
+    window.addEventListener("beforeunload", this.preventNav);
+    this.$once("hook:beforeDestroy", () => {
+      window.removeEventListener("beforeunload", this.preventNav);
+    });
+  },
+
+  beforeRouteLeave(to, from, next) {
+    if (this.isPlaying) {
+      if (!window.confirm("Abandon the game?")) {
+        return;
+      }
+    }
+    next();
+  },
+
   mounted() {
-    this.generate();
+    this.generate(null);
+  },
+  data: function () {
+    return {
+      isPlaying: false,
+      table: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      tableText: [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+    };
   },
   methods: {
-    generate() {
+    reset: function () {
+      this.isPlaying = false;
+      for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+          this.table[y][x] = 0;
+        }
+      }
+      this.table[2][2] = 1;
+      this.generate(this.tableText);
+    },
+    checkBingo: function () {
+      //check rows
+      let colSums=[0,0,0,0,0];
+      for (let y = 0; y < 5; y++) {
+        let rowSum = 0;
+        for (let x = 0; x < 5; x++) {
+          rowSum+=this.table[y][x];
+          colSums[x]+=this.table[y][x];
+          if (colSums[x] === 5) {
+            return true;
+          }
+        }
+        if (rowSum===5) {
+          return true;
+        }
+      }
+      return false;
+    },
+    mouseClick: function (event) {
+      if (event.offsetX && event.offsetY) {
+        let x = event.offsetX - 44;
+        let y = event.offsetY - 476;
+        if (x > 0 && y > 0 && x < 909 && y < 1350) {
+          this.isPlaying = true;
+          let tableX = Math.ceil(x / 164) - 1;
+          let tableY = Math.ceil(y / 164) - 1;
+          this.table[tableY][tableX] = 1;
+          {
+            //draw
+            const context = this.$refs.bingoCanvas.getContext("2d");
+
+            context.font = "16pt Arial";
+            let cellX = tableX*164+44;
+            let cellY = tableY*164+475;
+            let width = 161;
+            context.beginPath();
+            context.lineWidth="2";
+            context.strokeStyle = "red";
+            context.rect(cellX,cellY,width,width);
+            context.stroke();
+          }
+          if (this.checkBingo()) {
+            alert("Bingo!");
+          }
+        }
+      }
+    },
+    generate(generatedLines = null) {
+      if (this.isPlaying) {
+        if (!window.confirm("Abandon the game?")) {
+          this.reset();
+          return;
+        }
+      }
+
       const context = this.$refs.bingoCanvas.getContext("2d");
       context.canvas.width = 909;
       context.canvas.height = 1350;
@@ -26,19 +126,30 @@ export default {
       const bg = new Image();
       bg.onload = () => {
         context.drawImage(bg, 0, 0);
-        const lines = this.getRandomLines(bingoLines, 24);
+
+        const lines =
+          generatedLines === null
+            ? this.getRandomLines(bingoLines, 24)
+            : generatedLines;
+
         const xOrigin = 45;
         const yOrigin = 480;
         const squareSide = 165;
         const textBoxPadding = 5;
         const textBox = 160;
+
         for (let line = 0; line < 5; line++) {
           for (let col = 0; col < 5; col++) {
             let text;
-            if (col === 2 && line === 2) {
-              text = "Konlulu!";
+            if (generatedLines === null) {
+              if (col === 2 && line === 2) {
+                text = "Konlulu!";
+              } else {
+                text = lines.pop()[this.$i18n.locale];
+              }
+              this.tableText[line][col] = text;
             } else {
-              text = lines.pop()[this.$i18n.locale];
+              text = this.tableText[line][col];
             }
 
             this.wrapText(
@@ -76,7 +187,7 @@ export default {
       for (let n = 0; n < words.length; n++) {
         const testLine = line + words[n] + " ";
         const metrics = context.measureText(testLine);
-        
+
         const testWidth = metrics.width;
         if (testWidth > maxWidth && n > 0) {
           context.fillText(line, x, y);
@@ -88,11 +199,13 @@ export default {
       }
       context.fillText(line, x, y);
     },
-    download(){
+    download() {
       //
-      var image = this.$refs.bingoCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
-      window.location.href=image;
-    }
+      var image = this.$refs.bingoCanvas
+        .toDataURL("image/png")
+        .replace("image/png", "image/octet-stream"); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+      window.location.href = image;
+    },
   },
 };
 </script>
