7d4c043ae8a37c1bd4cc3c772604608da57d1032
nobooli <>
2021-08-06 21:55:06 -0400

    draft of messages with flags + pfps

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -32,7 +32,12 @@
                         <span :class="message.country_flag"></span>
                     </v-card-title>
                     <v-card-subtitle>{{ message.sns_tags }}</v-card-subtitle>
-                    <v-card-text class="message-body">
+                    <v-card-text class="message-body" v-if="message_jp === ''">
+                        {{ message.message }}
+                    </v-card-text>
+                    <v-card-text class="message-body" v-else>
+                        {{ message.message_jp }}
+                        <v-divider v-if="!(message.message_jp === '')"></v-divider>
                         {{ message.message }}
                     </v-card-text>
                 </v-card>
@@ -42,9 +47,6 @@
 </template>
 
 <script>
-import filler_cream from "../assets/img/zain-profile-pic-cream.svg";
-import filler_gray from "../assets/img/zain-profile-pic-gray.svg";
-
 import art_pieces from "../assets/art_pieces.json";
 import messages from "../assets/messages.json";
 
@@ -60,46 +62,6 @@ export default {
         return {
             art_pieces: art_pieces.art_pieces,
             messages: messages.messages,
-            // messages: [ // move to json file once I have real messages
-            //     {
-            //         name: "zain 1",
-            //         profile_pic: "../assets/img/zain-profile-pic-cream.svg",
-            //         country: "",
-            //         message: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in vel illum qui dolorem eum fugiat quo voluptas nulla pariatur."
-            //     },
-            //     {
-            //         name: "zain 2",
-            //         profile_pic: "../assets/img/zain-profile-pic-gray.svg",
-            //         country: "../assets/img/flag_cn.svg",
-            //         message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            //     },
-            //     {
-            //         name: "zain 3",
-            //         profile_pic: "../assets/img/zain-profile-pic-cream.svg",
-            //         country: "../assets/img/flag_jp.svg",
-            //         message: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex."
-            //     },
-            //     {
-            //         name: "zain 4",
-            //         profile_pic: "../assets/img/zain-profile-pic-cream.svg",
-            //         country: "../assets/img/flag_nl.svg",
-            //         message: "Sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet. Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-            //     },
-            //     {
-            //         name: "zain 5",
-            //         profile_pic: "../assets/img/zain-profile-pic-gray.svg",
-            //         country: "../assets/img/flag_ru.svg",
-            //         message: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quis."
-            //     },
-            //     {
-            //         name: "zain 6",
-            //         profile_pic: "../assets/img/zain-profile-pic-gray.svg",
-            //         country: "../assets/img/flag_tw.svg",
-            //         message: "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in vel illum qui dolorem eum fugiat quo voluptas nulla pariatur."
-            //     }
-            // ],
-            cream: filler_cream,
-            gray: filler_gray,
             options: {
                 closeText: "X",
                 captions: "true",
@@ -146,7 +108,12 @@ export default {
     white-space: pre;
 }
 
+.message-body > .v-divider {
+    margin: 1em;
+}
+
 .profile-pic {
     max-width: 64px;
+    max-height: 64px;
 }
 </style>
