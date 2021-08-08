d64e8e651cb0dbede9c93eaacd5eaa80de42b22d
nobooli <>
2021-08-08 09:29:25 -0400

    typo messing with hover

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -21,7 +21,7 @@
           <img :src="hovered_zain === -1 ? flag_hover : flag_idle"
             @mouseover="hovered_zain = -1"
             @mouseleave="hovered_zain = null"
-            :srcset="hovered_zain === -1 ? flag_srcset_hover : flag_srcset_hover"
+            :srcset="hovered_zain === -1 ? flag_srcset_hover : flag_srcset"
             :sizes="zain_sizes">
         </a>
       </v-hover>
@@ -49,7 +49,7 @@
         flag_idle: "img/flag_idle_300.png",
         flag_hover: "img/flag_hover_300.png",
         flag_srcset: "img/flag_idle_75.png 75w, img/flag_idle_150.png 150w, img/flag_idle_300.png 300w",
-        flag_srcset_hover: "img/flag_idle_75.png 75w, img/flag_idle_300.png 300w",
+        flag_srcset_hover: "img/flag_hover_75.png 75w, img/flag_hover_150.png 150w, img/flag_hover_300.png 300w",
         tent: "img/TEMT_WITH_BOWS_1000.png",
         tent_srcset: "img/TEMT_WITH_BOWS_250.png 250w, img/TEMT_WITH_BOWS_500.png 500w, img/TEMT_WITH_BOWS_1000.png 1000w",
         tent_sizes: "(max-width: 600px) 250px, (max-width: 1280px) 500px, 1000px",
@@ -60,7 +60,7 @@
             img_hover: "img/letter_hover_300.png",
             id: "letter-zain",
             srcset: "img/letter_idle_75.png 75w, img/letter_idle_150.png 150w, img/letter_idle_300.png 300w",
-            srcset_hover: ""
+            srcset_hover: "img/letter_hover_75.png 75w, img/letter_hover_150.png 150w, img/letter_hover_300.png 300w"
           },
           { // gallery
             link: "/Presents",
@@ -68,7 +68,7 @@
             img_hover: "img/gallery_hover_300.png",
             id: "gallery-zain",
             srcset: "img/gallery_idle_75.png 75w, img/gallery_idle_150.png 150w, img/gallery_idle_300.png 300w",
-            srcset_hover: ""
+            srcset_hover: "img/gallery_hover_75.png 75w, img/gallery_hover_150.png 150w, img/gallery_hover_300.png 300w"
           },
           { // soundboard
             link: "/Soundboard",
@@ -76,7 +76,7 @@
             img_hover: "img/megaphone_hover_300.png",
             id: "megaphone-zain",
             srcset: "img/megaphone_idle_75.png 75w, img/megaphone_idle_150.png 150w, img/megaphone_idle_300.png 300w",
-            srcset_hover: ""
+            srcset_hover: "img/megaphone_hover_75.png 75w, img/megaphone_hover_150.png 150w, img/megaphone_hover_300.png 300w"
           },
           { // Quiz
             link: "/Quiz",
@@ -84,7 +84,7 @@
             img_hover: "img/quiz_hover_300.png",
             id: "ball-zain",
             srcset: "img/quiz_idle_75.png 75w, img/quiz_idle_150.png 150w, img/quiz_idle_300.png 300w",
-            srcset_hover: ""
+            srcset_hover: "img/quiz_hover_75.png 75w, img/quiz_hover_150.png 150w, img/quiz_hover_300.png 300w"
           }
         ]
       }
