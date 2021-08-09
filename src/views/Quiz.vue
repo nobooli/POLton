8417551e013a3d183398488ad69267ac2b3514ba
nobooli <>
2021-08-09 00:16:44 -0400

    quiz iframe sized

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -2,5 +2,10 @@
     <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScmOsjndPd0qBXFVxhKIik3mxkgBlQAKUMyU_PAlTaBmTrWkg/formResponse?embedded=true"
         width="100%"
         height="100%"
-        frameborder="0"></iframe>
+        frameborder="0"
+        ></iframe>
 </template>
+
+<style>
+iframe { height: 100vh; }
+</style>
