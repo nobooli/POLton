485b0d49269a8c2eff11005ea282f415e8d83609
nobooli <>
2021-08-09 20:48:08 -0400

    removed (beta)

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -97,7 +97,7 @@ export default {
     langs: [
       { title: "English", src: "en", isready: true },
       //{ title: "简体中文", src: "zhHans", isready: false },
-      { title: "日本語(ベータ版)", src: "ja", isready: true },
+      { title: "日本語", src: "ja", isready: true },
     ],
   }),
   methods: {
