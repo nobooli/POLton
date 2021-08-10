8b25bc9205f67bc242d3a352afbef5a9518ae037
nobooli <>
2021-08-10 19:23:39 -0400

    Pre draft of animations on zains

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -90,7 +90,7 @@
             link: "/Quiz",
             img: "img/quiz_idle_300.png",
             img_hover: "img/quiz_hover_300.png",
-            id: "ball-zain",
+            id: "quiz-zain",
             srcset: "img/quiz_idle_75.png 75w, img/quiz_idle_150.png 150w, img/quiz_idle_300.png 300w",
             srcset_hover: "img/quiz_hover_75.png 75w, img/quiz_hover_150.png 150w, img/quiz_hover_300.png 300w"
           }
@@ -237,6 +237,156 @@
 @keyframes title_text {
   100% { 
     transform: translateX(-100%); 
+  }
+}
+
+.navi-zain img {
+	animation-duration: 8s/*1.6s*/;
+  animation-iteration-count: infinite;
+	transform-origin: bottom;
+  animation-timing-function: cubic-bezier(0.28, 0.84, 0.42, 1);
+}
+
+#flag-zain img {
+  animation-name: flag_bounce;
+}
+
+#letter-zain {
+  animation-name: letter_bounce;
+}
+
+#gallery-zain {
+  animation-name: gallery_bounce;
+}
+
+#megaphone-zain {
+  animation-name: megaphone_bounce;
+}
+
+#quiz-zain {
+  animation-name: quiz_bounce;
+}
+
+@media screen and (max-width: 959px)
+{
+  @keyframes flag_bounce {
+    0%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    2%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    6%  { transform: scale(.9,1.1)   translateY(-40px); } /* 1.6s, 30% */
+    10%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    11.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    12.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    20% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes letter_bounce {
+    /* 1.6 seconds of waiting */
+    20%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    22%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    26%  { transform: scale(.9,1.1)   translateY(-40px); } /* 1.6s, 30% */
+    30%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    31.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    32.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    40% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes gallery_bounce {
+    /* 3.2 seconds of waiting */
+    40%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    42%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    46%  { transform: scale(.9,1.1)   translateY(-40px); } /* 1.6s, 30% */
+    50%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    51.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    52.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    60% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes megaphone_bounce {
+    /* 4.8 seconds of waiting */
+    60%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    62%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    66%  { transform: scale(.9,1.1)   translateY(-40px); } /* 1.6s, 30% */
+    50%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    71.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    72.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    80% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes quiz_bounce {
+    /* 6.4 seconds of waiting */
+    80%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    82%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    86%  { transform: scale(.9,1.1)   translateY(-60px); } /* 1.6s, 30% */
+    70%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    91.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    92.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    100% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+}
+
+@media screen and (min-width: 960px) {
+  @keyframes flag_bounce {
+    0%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    2%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    6%  { transform: scale(.9,1.1)   translateY(-100px); } /* 1.6s, 30% */
+    10%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    11.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    12.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    20% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes letter_bounce {
+    /* 1.6 seconds of waiting */
+    20%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    22%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    26%  { transform: scale(.9,1.1)   translateY(-100px); } /* 1.6s, 30% */
+    30%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    31.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    32.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    40% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes gallery_bounce {
+    /* 3.2 seconds of waiting */
+    40%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    42%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    46%  { transform: scale(.9,1.1)   translateY(-100px); } /* 1.6s, 30% */
+    50%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    51.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    52.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    60% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes megaphone_bounce {
+    /* 4.8 seconds of waiting */
+    60%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    62%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    66%  { transform: scale(.9,1.1)   translateY(-100px); } /* 1.6s, 30% */
+    50%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    71.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    72.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    80% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
+  }
+
+  @keyframes quiz_bounce {
+    /* 6.4 seconds of waiting */
+    80%   { transform: scale(1,1)      translateY(0); } /* 1.6s, 0% */
+    82%  { transform: scale(1.1,.9)   translateY(0); } /* 1.6s, 10% */
+    86%  { transform: scale(.9,1.1)   translateY(-100px); } /* 1.6s, 30% */
+    70%  { transform: scale(1.05,.95) translateY(0); } /* 1.6s, 50% */
+    91.4%  { transform: scale(1,1)      translateY(-7px); } /* 1.6s, 57% */
+    92.8%  { transform: scale(1,1)      translateY(0); } /* 1.6s, 64% */
+    100% { transform: scale(1,1)      translateY(0); } /* 1.6s, 100% */
+    /* remainder of 8 seconds */
   }
 }
 </style>
