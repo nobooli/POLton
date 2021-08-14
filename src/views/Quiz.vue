205dcc41d11bf12dd3380317ed1b3778169a1504
Matthew Tao <goatatia@gmail.com>
2021-08-14 16:43:27 +1200

    added headers to pages with banner art

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,64 +1,65 @@
 <template>
-    <v-container>
-        <div class="main-title-card">
-          <div class="ticket_card">
-            <div>
-              <p class="title_text" style="margin-bottom:0px;">
-                <span>{{$t('link.quiz')}}</span>
-              </p>
-            </div>
-          </div>
-        </div>
-        <br />
-        <div data-aos="zoom-in" data-aos-delay="1500">
-          <!-- <iframe :src="'https://docs.google.com/forms/d/e/1FAIpQLScfRnEEdS-VSwQzkea_5OwV83NK5VzieqeDwl7n6wlnrJmL_A/viewform?usp=pp_url&entry.1439456151=' + uuid + '&embedded=true'" 
+	<v-container>
+		<div class="main-title-card">
+			<div class="ticket_card">
+				<div>
+					<p class="title_text" style="margin-bottom:0px;">
+						<span>{{ $t("link.quiz") }}</span>
+					</p>
+				</div>
+			</div>
+		</div>
+		<br />
+		<div data-aos="zoom-in" data-aos-delay="1500">
+			<!-- <iframe :src="'https://docs.google.com/forms/d/e/1FAIpQLScfRnEEdS-VSwQzkea_5OwV83NK5VzieqeDwl7n6wlnrJmL_A/viewform?usp=pp_url&entry.1439456151=' + uuid + '&embedded=true'" 
             width="640" 
             height="648" 
             frameborder="0" 
             marginheight="0" 
             marginwidth="0"
           >Wird geladen…</iframe> -->
-          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScmOsjndPd0qBXFVxhKIik3mxkgBlQAKUMyU_PAlTaBmTrWkg/formResponse?embedded=true"
-              width="100%"
-              height="100%"
-              frameborder="0"
-              >
-            <!-- <input hidden id="quizTakerId" name="quizTakerId" value="{{ uuid }}"> -->
-          </iframe>
-        </div>
-    </v-container>
+			<iframe
+				src="https://docs.google.com/forms/d/e/1FAIpQLScmOsjndPd0qBXFVxhKIik3mxkgBlQAKUMyU_PAlTaBmTrWkg/formResponse?embedded=true"
+				width="100%"
+				height="100%"
+				frameborder="0"
+			>
+				<!-- <input hidden id="quizTakerId" name="quizTakerId" value="{{ uuid }}"> -->
+			</iframe>
+		</div>
+	</v-container>
 </template>
 
 <script>
-import { v4 as uuidv4 } from 'uuid';
-import AOS from 'aos';
+import { v4 as uuidv4 } from "uuid";
+import AOS from "aos";
 
 export default {
-    setup() {
-        
-    },
-    methods: {
-      AOSInit() {
-        this.aos = AOS;
-        this.aos.init({
-          duration: 1500,
-          disable: "mobile"
-        });
-      }
-    },
-    mounted() {
-      this.$nextTick(() => {
-        this.AOSInit();
-      });
-    },
-    data() {
-      return {
-        uuid: uuidv4()
-      }
-    }
-}
+	setup() {},
+	methods: {
+		AOSInit() {
+			this.aos = AOS;
+			this.aos.init({
+				duration: 1500,
+				disable: "mobile",
+			});
+		},
+	},
+	mounted() {
+		this.$nextTick(() => {
+			this.AOSInit();
+		});
+	},
+	data() {
+		return {
+			uuid: uuidv4(),
+		};
+	},
+};
 </script>
 
 <style>
-iframe { height: 100vh; }
+iframe {
+	height: 100vh;
+}
 </style>
