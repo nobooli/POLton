599a4456ba96fedee91586080e009b591d29673e
nobooli <>
2021-08-13 21:57:45 -0400

    draft of credits footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,23 +1,40 @@
 <template>
     <div id="credits">
-        <div id="stageHands">
+        <div v-for="group in credits" :key="group.name"
+            class="credit-group"
+        >
+            <div class="credit-group-name">
+                <span>{{ resolveI18n(group.translation) }}</span>
+            </div>
+            <div v-for="(member, i) in group.members" :key="i"
+                class="credit-group-member"
+            >
+                <span>{{ member.name }}</span>
+            </div>
         </div>
-        <div id="website"></div>
-        <div id="sound"></div>
-        <div id="translationCredits"></div>
-        <div id="presentArt"></div>
-        <div id="presentLayoutDesign"></div>
-        <div id="art"></div>
-        <div id="specialThanks"></div>
-        <div id="disclaimer"></div>
+        <div>
+            <span>{{ $t('ui.disclaimer') }}</span>
+        </div>
     </div>
 </template>
 
 <script>
+import credits from "../assets/credits.json"
+
 export default {
-    
+    data: function() {
+        return {
+            credits: credits.credits
+        }
+    },
+    methods: {
+        resolveI18n(obj) {
+            return obj[this.$i18n.locale];
+        }
+    }   
 }
 </script>
 
 <style>
+
 </style>
